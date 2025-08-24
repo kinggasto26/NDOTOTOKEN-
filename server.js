c346a4497efd2b119a61cc80de1591b727b656c8
kinggasto26 <gastoemanuely20@gmail.com>
2025-08-24 10:50:22 +0300

    Update server.js

--- server.js
+++ server.js
@@ -28,6 +28,7 @@ app.get("/", (req, res) => {
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
+
 // ===== Admin Settings Routes =====
 let adminSettings = {
   ads: false,
