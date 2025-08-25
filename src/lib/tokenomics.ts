27c0251485d90ef8364fdc21d131a46d8fbf83a8
kinggasto26 <gastoemanuely20@gmail.com>
2025-08-25 07:00:52 +0300

    Create src/lib/tokenomics.ts

---
+++ src/lib/tokenomics.ts
@@ -0,0 +1,10 @@
+export const TOTAL_SUPPLY = 50000000000;
+
+export const TOKENOMICS = {
+  tapToEarn: 0.4 * TOTAL_SUPPLY,
+  adsReferrals: 0.14 * TOTAL_SUPPLY,
+  airdrop: 0.08 * TOTAL_SUPPLY,
+  staking: 0.15 * TOTAL_SUPPLY,
+  adminDev: 0.13 * TOTAL_SUPPLY,
+  liquidity: 0.1 * TOTAL_SUPPLY
+};
